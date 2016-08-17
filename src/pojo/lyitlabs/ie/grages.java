4ed50253496c57b70e4e7d0f819c5afcda1a63c2
EmmaMcnamee <L00106074@student.lyit.ie>
2016-08-17 13:10:00 +0100

    This is a comment from Emmas new commit

---
+++ src/pojo/lyitlabs/ie/grages.java
@@ -0,0 +1,25 @@
+/**
+ * @author Emma Mc Namee
+ * @Date 20/07/2016
+ * @Desc grades git practical
+ */
+package pojo.lyitlabs.ie;
+
+public class grades {
+		private int studentid = 123456789;
+		private String moduleid = "ENTPERF";
+		private double grade = 0.0;
+		
+		public double getGrade() {
+			return grade;
+		}
+		public void setGrade(double grade) {
+			this.grade = grade;
+		}
+		public int getStudentid() {
+			return studentid;//this code has just been added
+		}
+		public String getModuleid() {
+			return moduleid;
+		}
+}
